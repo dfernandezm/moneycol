520c742818f97f74244c2f8ff40e2fc371d76ed4
David Fernandez <morenza@gmail.com>
2019-04-17 23:04:06 +0100

    Separated elasticSearch bits into infrastructure folder

--- server/api/api.js
+++ server/api/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-const searcher = require("../elasticSearchImporter")
+const searcher = require("../infrastructure/search")
 
 const bodyParser = require('body-parser'); 
 
