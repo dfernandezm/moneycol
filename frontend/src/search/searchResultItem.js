c9582724d679a15a935d5b50b3ec578f689fd4db
David Fernandez <morenza@gmail.com>
2019-08-14 23:02:37 +0100

    added detail link

--- frontend/src/search/searchResultItem.js
+++ frontend/src/search/searchResultItem.js
@@ -3,7 +3,6 @@ import './css/searchResult.css';
 
 export default function SearchResultsItem({ item, index }) {
   return (
-
         <div className="card horizontal" key={index}>
             <div className="card-image">
                 <div className="image-wrapper">
