e96ce6e490b8b0c9aec5f89d4fc8bfbb310504ce
David Fernandez <morenza@gmail.com>
2019-11-04 10:44:06 +0000

    First version of elasticsearch chart

--- server/deploy/terraform/cluster.tf
+++ server/deploy/terraform/cluster.tf
@@ -54,7 +54,6 @@ resource "google_container_node_pool" "main_node_pool" {
   }
 }
 
-
 resource "google_container_node_pool" "es_node_pool" {
   provider   = "google-beta"
   name       = "elasticsearch-pool"
@@ -82,27 +81,3 @@ resource "google_container_node_pool" "es_node_pool" {
 }
 
 # https://www.edureka.co/blog/kubernetes-dashboard/
-
-# resource "google_container_node_pool" "main" {
-#   name       = "main"
-#   zone       = "${var.cluster_zone}"
-#   cluster    = "${google_container_cluster.main.name}"
-#   node_count = 1
-
-#   management = {
-#     auto_repair  = true
-#     auto_upgrade = true
-#   }
-
-#   node_config {
-#     preemptible  = true
-#     machine_type = "n1-standard-2"
-
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/compute",
-#       "https://www.googleapis.com/auth/devstorage.read_only",
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
-#   }
-# }
