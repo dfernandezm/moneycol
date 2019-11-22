ae05dc1a0a9f47ba84887c476e58dae3e63a1c50
David Fernandez <morenza@gmail.com>
2019-11-22 17:47:46 +0000

    WIP

---
+++ frontend/src/home/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home: React.FC<{}> = () => {
+
+  return (
+    <div className="home">
+      <h3> Welcome </h3>
+    </div>
+  );
+
+}
---
+++ frontend/src/main.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Route,
+  BrowserRouter,
+  Switch,
+  withRouter
+} from "react-router-dom";
+
+import 'materialize-css/dist/css/materialize.min.css';
+
+import SearchResultsPage from './search/searchResultsPage';
+import Home from './home/home';
+import NavBar from './navbar/navBar';
+
+const Main: React.FC<{}> = () => {
+  return (
+    <BrowserRouter>
+
+      <div className="mainpage">
+        <div className="section no-pad-bot mainContent">
+          <Switch>
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/searchResultsPage" component={withRouter(SearchResultsPage)} />
+          </Switch>
+        </div>
+      </div>
+
+    </BrowserRouter>
+  );
+}
+
+export { Main };
