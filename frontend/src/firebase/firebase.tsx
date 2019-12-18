7dfd705f27019950ac98299727325de10820dd3a
David Fernandez <morenza@gmail.com>
2019-12-18 18:28:12 +0100

    Firebase config from server cleanup

--- frontend/src/firebase/firebase.tsx
+++ frontend/src/firebase/firebase.tsx
@@ -2,17 +2,26 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-//TODO: try to remove this
-const firebaseConfig = {
-    apiKey: "AIzaSyDImTA3-o5ew92DQ4pg0-nVKTHR92ncq-U",
-    authDomain: "moneycol.firebaseapp.com",
-    databaseURL: "https://moneycol.firebaseio.com",
-    projectId: "moneycol",
-    storageBucket: "moneycol.appspot.com",
-    messagingSenderId: "461081581931",
-    appId: "1:461081581931:web:3ca5344ae0e1df6dfa542e"
-  };
+const FIREBASE_CONFIG_URL = process.env.NODE_ENV == 'production' ? "api/firebaseConfig" : 
+                                    "http://localhost:4000/api/firebaseConfig"
 
-export const myFirebase = firebase.initializeApp(firebaseConfig);
-const baseDb = myFirebase.firestore();
-export const db = baseDb;
+const findFirebaseConfig = async () => {
+  const response = await fetch(FIREBASE_CONFIG_URL);
+  return response.json();
+}
+
+let firebaseConfig = "";
+let firebaseApp: any = {};
+
+export const myFirebase = async () => {
+  if (!firebaseConfig) {
+    firebaseConfig = await findFirebaseConfig()
+  }
+ 
+  if (!firebaseApp.auth) {
+    firebaseApp = firebase.initializeApp(firebaseConfig);
+    const baseDb = firebaseApp.firestore();
+  }
+  
+  return firebaseApp;
+}
