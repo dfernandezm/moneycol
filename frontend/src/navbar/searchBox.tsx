fee9de935e3039ddbe0806d80f69a63ac38ac620
David Fernandez <morenza@gmail.com>
2019-12-20 14:17:10 +0100

    WIP: changed results, need to do login form

--- frontend/src/navbar/searchBox.tsx
+++ frontend/src/navbar/searchBox.tsx
@@ -1,5 +1,49 @@
 import React from 'react';
-import './searchBox.css';
+import { fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: fade(theme.palette.common.white, 0.15),
+            '&:hover': {
+                backgroundColor: fade(theme.palette.common.white, 0.25),
+            },
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                marginLeft: theme.spacing(1),
+                width: 'auto',
+            },
+        },
+        searchIcon: {
+            width: theme.spacing(7),
+            height: '100%',
+            position: 'absolute',
+            pointerEvents: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        inputRoot: {
+            color: 'inherit',
+        },
+        inputInput: {
+            padding: theme.spacing(1, 1, 1, 7),
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: 120,
+                '&:focus': {
+                    width: 200,
+                },
+            },
+        }
+    })
+);
 
 type SearchBoxProps = {
     onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
@@ -10,23 +54,22 @@ type SearchBoxProps = {
 
 const SearchBox: React.FC<SearchBoxProps> =
     ({ onSubmit, onChange, searchTerm, placeholderText = "Search the catalog" }) => {
-
+        const classes = useStyles();
         return (
-            <div className="searchBox__">
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <SearchIcon />
+                </div>
                 <form onSubmit={onSubmit}>
-                    <div className="searchBox__input input-field">
-                        <input id="search"
-                            type="search"
-                            name="searchTerm"
-                            onChange={onChange}
-                            value={searchTerm}
-                            placeholder={placeholderText} />
-                        <label
-                            className="label-icon active"
-                            htmlFor="search">
-                            <i className="material-icons">search</i>
-                        </label>
-                    </div>
+                    <InputBase
+                        onChange={onChange}
+                        placeholder={placeholderText}
+                        value={searchTerm}
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }} />
                 </form>
             </div>
         );
--- frontend/src/search/searchResultItem.tsx
+++ frontend/src/search/searchResultItem.tsx
@@ -1,34 +1,75 @@
 import React from 'react';
-import './css/searchResult.css';
+
 import { SearchResult } from './types/SearchResult';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import Grid from '@material-ui/core/Grid';
+
+
+const useStyles = makeStyles({
+  card: {
+    width: "100%",
+    marginBottom: 10
+  },
+});
 
 type SearchResultsItemProps = {
     item: SearchResult,
     index: number
 }
 
-const SearchResultsItem: React.FC<SearchResultsItemProps> = ({ item, index }) => {
-    return (
-        <div className="card horizontal" key={item.catalogCode}>
-            <div className="card-image">
-                <div className="image-wrapper">
-                    <img src={item.imageFront} alt="img" />
-                </div>
-            </div>
-            <div className="card-stacked">
-                <div className="card-content">
-                    <h3>{item.country} - {item.banknoteName}</h3>
-                    <h4>{item.year}</h4>
-                    <p>{item.description}</p>
-                    <p><a href={item.detailLink} target="_blank">Detail</a></p>
-                </div>
-                <div className="card-action">
-                    <a href="#" className="green-link">Add to collection</a>
-                </div>
-            </div>
-        </div>
-    )
+const SearchResultItem: React.FC<SearchResultsItemProps> = ({item, index}) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      
+        <Grid container spacing={3}>
+            <Grid item xs={6}>
+                <CardMedia
+                    component="img"
+                    alt="image"
+                    height="300"
+                    image={item.imageFront}
+                    title="image"
+                />
+            </Grid>
+            <Grid item xs={6}>
+                <CardContent>
+                    <Typography gutterBottom variant="h4" component="h2">
+                        {item.country} - {item.banknoteName}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {item.year}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {item.description}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        <a href={item.detailLink} target="_blank">Detail</a>
+                    </Typography>
+                </CardContent>
+            </Grid>
+            <Grid item xs={12}>
+                <CardActions>
+                    <Button size="small" color="primary">
+                    Share
+                    </Button>
+                    <Button size="small" color="primary">
+                    Add to Collection
+                    </Button>
+                </CardActions>
+            </Grid>
+        </Grid>
+    </Card>
+  );
 }
 
-export default SearchResultsItem;
+export default SearchResultItem;
 
