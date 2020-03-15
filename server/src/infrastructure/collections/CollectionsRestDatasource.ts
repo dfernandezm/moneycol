a45dd85cb27ab69d274ad189a3a090fa2717f9b1
David Fernandez <morenza@gmail.com>
2020-03-15 15:57:25 +0000

    #103 Create collection resolver connected to API

--- server/src/infrastructure/collections/CollectionsRestDatasource.ts
+++ server/src/infrastructure/collections/CollectionsRestDatasource.ts
@@ -1,5 +1,13 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
 import { CollectionApiResult } from './types';
+import { NewCollectionInput } from '../SearchResult';
+import { CollectionCreatedResult } from '../SearchResult';
+import {
+  Request
+} from 'apollo-server-env';
+import {
+  ApolloError
+} from 'apollo-server-errors';
 
 export class CollectionsRestDatasource extends RESTDataSource {
   
@@ -15,4 +23,18 @@ export class CollectionsRestDatasource extends RESTDataSource {
   async getItemsForCollection(collectionId: string): Promise<CollectionApiResult> {
     return this.get(`collections/${collectionId}`);
   }
+
+  async createCollection(collection: NewCollectionInput): Promise<CollectionCreatedResult> {
+    return this.post(`collections`, collection);
+  }
+
+  //TODO: workaround for now, the server should return this already. Created bug #130 for this
+  protected didEncounterError(error: ApolloError, _request: Request) {
+    let message = error.extensions.response.body.message;
+    console.log("Error from API", message);
+    error.extensions.code = "BAD_REQUEST";
+    error.extensions.response.status = 400;
+    throw error;
+  }
+
 };
