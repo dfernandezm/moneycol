efa4d705c1c2a64b00180c2ca28291ec7d9faa62
David Fernandez <morenza@gmail.com>
2020-07-02 08:21:42 +0100

    Closes #212 Basic user profile form

--- frontend/src/login/localState/localStateService.ts
+++ frontend/src/login/localState/localStateService.ts
@@ -1,5 +1,41 @@
-class LocalStateService {
+type User = {
+    userId: string,
+    token: string,
+    email: string
+}
 
+class LocalStateService {
+    setToken(token: string) {
+        localStorage.setItem("token", token);
+    }
+
+    getToken(): string | null {
+        return localStorage.getItem("token");
+    }
+
+    getUser(): User | null {
+        const localUser = localStorage.getItem("user");
+        if (localUser) {
+            return JSON.parse(localUser) as User;
+        }
+        return null;
+    }
+
+    setUserFromObject(userObj: User) {
+        localStorage.setItem("user", JSON.stringify(userObj));
+    }
+
+    clear() {
+        localStorage.clear();
+    }
+
+    clearToken() {
+        localStorage.removeItem("token");
+    }
+
+    clearUser() {
+        localStorage.removeItem("user");
+    }
 }
 
 export const localStateService = new LocalStateService();
---
+++ frontend/src/users/userprofile/updateUserProfileForm.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+import { Formik, Form, FormikHelpers, FormikProps } from 'formik';
+
+import { useMutation } from "@apollo/react-hooks";
+import { UPDATE_USER_PROFILE_MUTATION } from "./gql/updateUserProfile";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+
+import { localStateService } from '../../login/localState/localStateService'
+
+// Formik form values
+interface Values {
+  username: string | '' | undefined;
+  firstName: string | '' | undefined;
+  lastName: string | '' | undefined
+}
+
+/**
+ * The form where users can update their profile information. This is a protected route, therefore
+ * users will be prompted to login if accessed directly
+ * 
+ */
+const UpdateUserProfileForm: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
+
+  const [updateUserProfile] = useMutation(UPDATE_USER_PROFILE_MUTATION);
+
+  //TODO: We need to pre-fill the form with user profile data,
+  // we may just store in localState the minimum (userId, email)
+  // and do a query to GQL to get the full profile to update
+  const user = localStateService.getUser();
+
+  //TODO: change these values with what comes from the GQL query to get user profile
+  const initialValues: Values = { 
+    username: user?.email, 
+    firstName: user?.email, 
+    lastName: user?.email 
+  };
+
+  const performValidation = (values: Values) => {
+    const errors: Partial<Values> = {};
+    if (!values.username) {
+        errors.username = 'Required';
+    } 
+    return errors;
+  }
+
+  /**
+   * Formik submission handler.  
+   * 
+   * @param values {Values} the form values to submit
+   * @param formikHelpers {FormikHelpers<Values>} in this case <code>setStatus</code> to set the form status after successful / failed submission
+   * and <code>setSubmitting</code> to change submission state both on error or success
+   */
+  const handleSubmit = async (values: Values, { setStatus, setSubmitting }: FormikHelpers<Values>) => {
+    try  {
+
+      //TODO: pass the userId from the stored user properly (not optional)
+      const userInput = {...values, userId: user?.userId};
+      const { data } = await updateUserProfile({ variables: { userInput: userInput }});
+     
+      console.log("Updated user", data.updateUserProfile);
+
+      setSubmitting(false);
+      props.history.replace("/protected");
+
+    } catch (err) {
+      // Repeat setSubmitting call instead of finally block otherwise React complains
+      // about changing state in an unmounted component
+      setSubmitting(false)
+      console.log(err);
+      setStatus("Error updating user profile: " + err.message);
+    }
+  }
+
+  return (  
+        <Formik 
+          initialValues={initialValues}
+          validate={performValidation}
+          onSubmit={handleSubmit}
+          validateOnChange={false}
+          component={InnerUserProfileForm} />
+  );
+}
+
+const InnerUserProfileForm = ({ 
+  handleSubmit, 
+  handleChange, 
+  values, 
+  errors, 
+  isSubmitting, 
+  status }: FormikProps<Values>) => (
+
+  <Form onSubmit={handleSubmit}>
+
+        <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            onChange={handleChange}
+            value={values.username}
+          />
+          
+          { //TODO: component for errors
+            // or style for errors: https://material-ui.com/components/text-fields/ 
+            errors.username ? <div>{errors.username}</div> : null
+          }
+      
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="firstName"
+            label="First name"
+            id="firstName"
+            onChange={handleChange}
+            value={values.firstName}
+          />
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="lastName"
+            label="Last name"
+            id="lastName"
+            onChange={handleChange}
+            value={values.lastName}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+            >
+            { isSubmitting ? "Updating" : "Update" }  
+          </Button>
+          { !!status && <div> {status} </div>}
+    </Form>
+)
+
+export default withRouter(UpdateUserProfileForm);
