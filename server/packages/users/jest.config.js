372d196a2c82a31a145d1519c3d7902c9c622450
David Fernandez <morenza@gmail.com>
2020-08-31 01:12:15 +0200

    Fixed tests and module interop issue by adapting ts-node and jest

--- server/packages/users/jest.config.js
+++ server/packages/users/jest.config.js
@@ -1,4 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+// Load the config which holds the path aliases.
+const { compilerOptions } = require("../../tsconfig.json");
+
 module.exports = {
-  preset: 'ts-jest',
+  preset: "ts-jest",
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    // This has to match the baseUrl defined in tsconfig.json.
+    prefix: "<rootDir>/../../",
+  }),
 };
