38f1f5bd2859277effd1cdbc3cbd4eecc8616668
David Fernandez <morenza@gmail.com>
2021-03-21 22:19:08 +0000

    Attempt to fix tests by increasing delays to check values in Firestore

--- collections/server/src/test/java/com/moneycol/collections/server/CollectionApplicationServiceTest.java
+++ collections/server/src/test/java/com/moneycol/collections/server/CollectionApplicationServiceTest.java
@@ -308,7 +308,7 @@ public class CollectionApplicationServiceTest {
         FirestoreHelper.createCollection(aCollectionId, collectionName, collectionDescription, collectorId);
 
         // This is a big delay, but without it the collection added is not found when finding it
-        delaySecond(2);
+        delaySecond(3);
         FirebaseCollectionRepository collectionRepository = new FirebaseCollectionRepository(firestoreProvider);
 
         // When: adding an item to it
@@ -555,6 +555,7 @@ public class CollectionApplicationServiceTest {
                 .build();
 
         cas.createCollection(createCollectionCommand);
+        delaySecond(1);
 
         // Then
         List<Map<String, Object>> allEvents = FirestoreHelper.findAllEvents();
