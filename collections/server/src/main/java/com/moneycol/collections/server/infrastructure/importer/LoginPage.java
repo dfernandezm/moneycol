99b4264818db4ee23c3067d55aaecf76f08eb3a9
David Fernandez <morenza@gmail.com>
2021-06-19 16:46:51 +0100

    Importer colnect PoC

---
+++ collections/server/src/main/java/com/moneycol/collections/server/infrastructure/importer/LoginPage.java
@@ -0,0 +1,8 @@
+package com.moneycol.collections.server.infrastructure.importer;
+
+public class LoginPage {
+
+    public void login() {
+
+    }
+}
---
+++ collections/server/src/main/java/com/moneycol/collections/server/infrastructure/importer/model/BanknoteVariant.java
@@ -0,0 +1,4 @@
+package com.moneycol.collections.server.infrastructure.importer.model;
+
+public class BanknoteVariant {
+}
