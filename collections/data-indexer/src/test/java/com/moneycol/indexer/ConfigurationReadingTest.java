14f16dc1c9490f065e85a4a4e6785e44b16e1644
David Fernandez <morenza@gmail.com>
2021-10-08 08:37:02 +0100

    To be fixed nullpointer - modifying test

--- collections/data-indexer/src/test/java/com/moneycol/indexer/ConfigurationReadingTest.java
+++ collections/data-indexer/src/test/java/com/moneycol/indexer/ConfigurationReadingTest.java
@@ -20,7 +20,8 @@ public class ConfigurationReadingTest {
                 .and("FANOUT_PUB_SUB_TRIGGER_TOPIC_NAME", "test.moneycol.indexer.batches")
                 .and("FANOUT_PUB_SUB_DONE_TOPIC_NAME", "test.moneycol.indexer.batching.done")
                 .and("FANOUT_PUB_SUB_SINK_TOPIC_NAME", "test.moneycol.indexer.sink")
-                //FANOUT_CONSOLIDATION_PROCESS_TIMEOUT_SECONDS
+                .and("FANOUT_CONSOLIDATION_PROCESS_TIMEOUT_SECONDS", "540")
+                .and("FANOUT_CONSOLIDATION_PROCESS_TIMEOUT_THRESHOLD_SECONDS", "10")
                 .execute(() -> {
 
                     ApplicationContext applicationContext = ApplicationContext.run();
