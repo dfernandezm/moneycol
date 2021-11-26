69503da0c5ccb756a2794befd94bc81bbba9cdee
David Fernandez <morenza@gmail.com>
2021-11-26 00:20:04 +0000

    Integration test with DI to auth against GKE

--- collections/data-indexer/src/main/java/com/moneycol/indexer/infra/connectivity/KubeClientApiFactory.java
+++
@@ -1,9 +0,0 @@
-package com.moneycol.indexer.infra.connectivity;
-
-//TODO: instantiate here the ApiClient
-
-import io.micronaut.context.annotation.Factory;
-
-@Factory
-public class KubeClientApiFactory {
-}
---
+++ collections/data-indexer/src/main/java/com/moneycol/indexer/infra/connectivity/gke/config/KubeApiClientFactory.java
@@ -0,0 +1,2 @@
+package com.moneycol.indexer.infra.connectivity;public class KubeApiClientFactory {
+}
